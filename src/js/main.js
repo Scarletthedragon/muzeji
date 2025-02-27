a6094b661e77facac44ad1a29742d716a6943364
Scarletthedragon <sarlote.termane@gmail.com>
2025-02-27 16:53:52 +0200

    qr to js

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const qrContainer = document.getElementById("qr");
+    if (qrContainer) {
+        new QRCode(qrContainer, {
+            text: window.location.href,
+            width: 128,
+            height: 128
+        });
+    }
+});
